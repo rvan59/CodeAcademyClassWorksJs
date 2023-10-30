a575e8d1192a03f8a35a87ae0ffeaff150d51d13
nargizvaliyeva <gd6w7pj92@code.edu.az>
2023-10-30 19:54:04 +0400

    yeni task

---
+++ 01.data-types&operators/appss.js
@@ -0,0 +1,45 @@
+// task1
+
+
+let firstName = "Revan";
+let lastName = "Muxdarov";
+let fullname = firstName + " " + lastName;
+
+console.log(fullname)
+//task2
+let country = "Terter";
+let age = "19";
+let job = "study";
+console.log("l.m from "+country +" l am " +age+ " years old"+" l am a" +job)
+
+//TASK3
+let number = 16; //number
+
+console.log( typeof number);
+let color = "Brown"; //
+console.log( typeof color);
+let link = true ; //
+console.log( typeof link);
+
+let x={
+    firstName: "Nick" ,
+    color: "Red" ,
+};
+//task4
+let distance = 8600;
+let speed = 120;
+let duration = distance / speed;
+console.log(duration*60)
+//task5
+let year = "2023";
+let birth = "2004";
+let cvb = year -" " -birth;
+console.log(cvb)
+
+//task6
+let ruslan = 15;
+let Revan = 19;
+let gulnur = 30;
+let aysel = 21;
+let cemi = ruslan +Revan+gulnur+aysel;
+console.log(cemi)
