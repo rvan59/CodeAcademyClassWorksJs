b2224df3129fb4d769e898438d5ee116d2ab64ea
nargizvaliyeva <gd6w7pj92@code.edu.az>
2023-10-31 19:45:26 +0400

    yeni task

---
+++ 02.data-types&operators/appss.js
@@ -0,0 +1,21 @@
+// task1
+
+// let hava = 22;
+
+// if(hava >22){
+//     console.log("Qalin geyin");
+// } else if (hava >15){
+//     console.log("Pencek geyin");
+// }
+// else if (hava <15) {
+//     console.log("Nazik geyin");
+// }
+
+// // task2
+// let letter = "u";
+
+// if (letter == "a" || letter == "i" || letter == "o" || letter == "u") {
+//   console.log(`${letter} sait herfdir`);
+// }
+// else{ console.log(`${letter} samit herifdir`);}
+
