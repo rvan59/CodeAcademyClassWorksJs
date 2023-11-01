18cac14c954de8b2613a7158e4c53d02aecb6c55
nargizvaliyeva <gd6w7pj92@code.edu.az>
2023-11-01 19:47:06 +0400

    yeni task

---
+++ 03.control-lupus/appss.js
@@ -0,0 +1,61 @@
+
+// task1
+// let count = 0
+// for (let i = 1; i < 100; i++){
+//   if (i % 2 === 0) console.log(i);
+//      }
+// // task2
+// let str = "Revan Muxdarov";
+// console.log(str[str.length - 1]);
+// console.log(str.length);
+
+// for (i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+// // task3
+// let count =0
+// for(let i=1 ; i<100 ; i++ ){
+// if(i%10===7)
+//     console.log(i);
+// }
+
+
+// }
+// // task4
+// let count=0
+// for (let i=1; i<100; i++){
+//     if(i%11===0)  console.log(i);
+// }
+// // task5
+// let n=15
+// for(let i=1; i<15; i++){
+//     if(n%i===0){
+//         console.log(`${i}`);
+//     }
+// }
+
+// // task6
+// let count = 5;
+// let f=1;
+//   for (let i = 1; i < count; i++){  
+//         f= f*i
+//       } 
+//       console.log("fact", f);
+
+// // task7
+// let count = 0
+//  for (let i = 1; i < 10; i++){
+//     if(i%2===0) {
+//         count= count+i
+//     }
+//      } console.log("count", count);
+
+
+// task8
+let count =0
+for(let i=10; i<50; i++ ){
+if(i%3===0)
+count = count + i;
+count += i;
+console.log(`${i}`);
+}
