87a666de654391cb8eb87304006ba669e9cee16d
rvan59 <muxdarovrvan26@gmail.com>
2023-11-03 09:02:37 -0700

    task yaradildi

---
+++ 05.Array&Object/appss.js
@@ -0,0 +1,27 @@
+// task1
+// const number=[12,45,6,78,34,99,23];
+
+// let max = number[0];
+// let min = number[0];
+
+
+// for (let i = 1; i < number.length; i++) {
+//   if (number[i] > max) {
+//     max = number[i];
+//   }
+//    else if (number[i] < min) {
+//     min = number[i];
+//   }
+// }
+
+// console.log("MAX", max);
+// console.log("MIN",min);
+
+// task2
+let number = [12,45,6,78,99,33]
+let sun =0
+for (let i=0; i<number.length; i++ ){
+
+sun +=number[i];
+}
+console.log(sun);
