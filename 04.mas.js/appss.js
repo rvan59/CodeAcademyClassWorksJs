5370102f1d398c9a2529840bbc2df45a254c7e34
rvan59 <muxdarovrvan26@gmail.com>
2023-11-02 23:35:32 -0700

    yeni task yaradildi

--- 04.mas.js/appss.js
+++
@@ -1,5 +0,0 @@
-let number=15.75
-console.log(Math.ceil(number));
-
-console.log(Math.floor(Math.random()*10));
-// devamini yaz
---
+++ 04.math.js/appss.js
@@ -0,0 +1,19 @@
+let number=15.75
+
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.round(number));
+console.log(Math.trunc(number));
+console.log("MAX", Math.max(13,45,74,63,-26));
+console.log("MIn", Math.min(13,45,74,63,-26));
+console.log(Math.sign(-10));
+console.log(Math.sign(0));
+console.log(Math.sign(10));
+console.log(Math.pow(2,5));
+console.log(Math.sqrt(81));
+console.log(Math.abs(-90));
+console.log(Math.random()*10);
+console.log(Math.round(Math.random()*10));
+number>=18 ? console.log("hello"):console.log("hi");
+number>=13 ? console.log("hello"):console.log("hi");
+number>=13 && console.log("hello");
