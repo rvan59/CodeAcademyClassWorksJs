4ecf75c5ddbf9e90471362c72683b2de72097093
rvan59 <muxdarovrvan26@gmail.com>
2023-11-07 05:56:29 -0800

    yeni task

---
+++ 07.JS.ANONYMOUS/appss.js
@@ -0,0 +1,13 @@
+// const number = [1, 2, 3, 4, 5, 6, 7, 8];
+// // task1
+// let tek = number.filter((item)=>{
+//     return item%2===1;
+// })
+// console.log(tek);
+
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let araliq = array.filter((item) => {
+
+   return item>0 && item<=10;
+  });
+  console.log(araliq);
