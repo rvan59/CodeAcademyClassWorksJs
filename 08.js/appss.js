37150fd02340cafb09a56b7df8da068cdc98e8f9
rvan59 <muxdarovrvan26@gmail.com>
2023-11-14 07:59:29 -0800

    yeni task

---
+++ 08.js/appss.js
@@ -0,0 +1,36 @@
+// // task1
+// const text = "Java is awesome. Java is fun.";
+// console.log(text.replaceAll("Java", "Javascript "));
+// // task2
+
+// function mixUp(a, b) {
+//    console.log(b.slice(0,2)+a.slice(2) + " "+a.slice(0,2)+b.slice(2));
+// }
+
+// mixUp("val", "pur");
+// console.log(mixUp("donce", "gevlet"));
+// // task3
+
+// function verbing(word) {
+//   let result;
+//   if (word.length < 3) {
+//     result = word;
+//   } else if (!word.endsWith("ing")) {
+//     result = word + "ing";
+//   } else {
+//     result = word + "ly";
+//   }
+//   return result;
+// }
+
+// console.log(verbing("go")); // go
+// console.log(verbing("swim")); //swiming
+// console.log(verbing("swiming")); //swimingly
+// // task4
+// function isPalindrome(string) {
+//     result = string.split("").reverse().join("")
+//     return string===result ? "palidrondur": "paluduron deyil"
+// }
+// console.log(isPalindrome("hello"));
+
+// console.log(isPalindrome("ses"));
