895cc00d22d72bcc0a806cefeda0c900f4678d12
rvan59 <muxdarovrvan26@gmail.com>
2023-11-15 07:46:17 -0800

    yeni task

---
+++ 09.object/object.js
@@ -0,0 +1,30 @@
+let object = {
+  firstName: "Revan",
+  lastName: "muxdarov",
+  age: "19",
+  skills: "hecne",
+  uni: "GDU",
+  sity: "Terter",
+};
+// // 1
+// console.log(Object.keys(object));
+// // 2
+// console.log(Object.values(object));
+// // 3
+// console.log(object);
+// // 4
+// delete object.age;
+// console.log(object);
+
+// // 5
+// let arry = Object.values(object);
+// let max = arry[0];
+
+// for (let i = 0; i < arry.length; i++) {
+//   if (max < arry[i]) {
+//     max = arry[i];
+//   }
+// }
+
+// console.log(max);
+
